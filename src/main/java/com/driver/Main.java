58e4a280cd26f7c46b7990bc0576bf9efd86c4d1
PALLERLAAKSHAY123 <139306847+PALLERLAAKSHAY123@users.noreply.github.com>
2026-01-06 21:39:57 +0530

    Add Product class with overloaded product methods

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-
+   public class Product{
+     public int product(int x, int y) {
+       return x*y;
+     }
+     public int product(int x, int y, int z) {
+       return x*y*z;
+     }
+     public double product(double x, double y) {
+       return x*y;
+     }
+     
+   }
+  Product p=new Product();
+  p.product(3,4);
+  p.product(2,4,6);
+  p.product(2.5,5.5);
+  
 }
